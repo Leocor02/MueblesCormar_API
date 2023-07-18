5295dce301159b37a447f1e39defe346cd44a1f5
Leonardo Cortes <cortesleo02@gmail.com>
2023-07-18 17:57:41 -0600

    Creación del DTO del usuario

---
+++ MueblesCormar_API/Models/DTOs/UsuarioDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace MueblesCormar_API.Models.DTOs
+{
+    public class UsuarioDTO
+    {
+        public int Idusuario { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Contrasennia { get; set; } = null!;
+        public string Telefono { get; set; } = null!;
+        public int IdrolUsuario { get; set; }
+    }
+}
