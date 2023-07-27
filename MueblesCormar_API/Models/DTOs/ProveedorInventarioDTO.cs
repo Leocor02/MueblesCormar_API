c13dcbba099faa77def74135828367f1b0ab412c
Leonardo Cortes <cortesleo02@gmail.com>
2023-07-26 18:21:22 -0600

    Creación del DTO de ProveedorInventario

---
+++ MueblesCormar_API/Models/DTOs/ProveedorInventarioDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MueblesCormar_API.Models.DTOs
+{
+    public class ProveedorInventarioDTO
+    {
+        public int IdproveedorInventario { get; set; }
+        public int Idproveedor { get; set; }
+        public int Idproducto { get; set; }
+    }
+}
