16143343850020b57ebca6aaea11914b83ee2cc2
Leonardo Cortes <cortesleo02@gmail.com>
2023-07-28 22:12:07 -0600

    Creación del Getdata y GetList de ProveedorInventario

--- MueblesCormar_API/Controllers/ProveedorInventariosController.cs
+++ MueblesCormar_API/Controllers/ProveedorInventariosController.cs
@@ -7,12 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MueblesCormar_API.Attributes;
 using MueblesCormar_API.Models;
+using MueblesCormar_API.Models.DTOs;
 
 namespace MueblesCormar_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [ApiKey]
+    //[ApiKey]
     public class ProveedorInventariosController : ControllerBase
     {
         private readonly MueblesCormarContext _context;
@@ -49,6 +50,103 @@ namespace MueblesCormar_API.Controllers
             }
 
             return proveedorInventario;
+        }
+
+        // GET: api/ProveedorInventarios/GetDataProveedorInventario?idProveedorInventario=1
+        [HttpGet("GetDataProveedorInventario")]
+        public ActionResult<IEnumerable<ProveedorInventarioDTO>> GetDataProveedorInventario(int idProveedorInventario)
+        {
+            if (_context.ProveedorInventarios == null)
+            {
+                return NotFound();
+            }
+            //las consultas linq se parecen mucho a las normales que hemos hecho en T-SQL
+            //una de las diferencias es que podemos usar una "tabla temporal" para almacenar
+            //los resultados y luego usarla para llenar los atributos de un modelo o DTO
+
+            var query = (from pi in _context.ProveedorInventarios
+                         join p in _context.Proveedors on pi.Idproveedor equals p.Idproveedor
+                         join i in _context.Inventarios on pi.Idproducto equals i.Idproducto
+                         where pi.IdproveedorInventario == idProveedorInventario
+                         select new
+                         {
+                             IdProveedorInventario = pi.IdproveedorInventario,
+                             Idproveedor = p.Idproveedor,
+                             Idproducto = i.Idproducto,
+                             NombreProveedor = p.Nombre,
+                             NombreProducto = i.Nombre
+                         }).ToList();
+
+            List<ProveedorInventarioDTO> list = new List<ProveedorInventarioDTO>();
+
+            foreach (var proveedorInventario in query)
+            {
+                ProveedorInventarioDTO NewItem = new ProveedorInventarioDTO();
+
+                NewItem.IdproveedorInventario = proveedorInventario.IdProveedorInventario;
+                NewItem.Idproveedor = proveedorInventario.Idproveedor;
+                NewItem.Idproducto = proveedorInventario.Idproducto;
+                NewItem.NombreProveedor = proveedorInventario.NombreProveedor;
+                NewItem.NombreProducto = proveedorInventario.NombreProducto;
+
+                list.Add(NewItem);
+
+            }
+
+            if (list == null)
+            {
+                return NotFound();
+            }
+
+            return list;
+
+        }
+
+        // GET: api/ProveedorInventarios/GetListaProveedorInventario
+        [HttpGet("GetListaProveedorInventario")]
+        public ActionResult<IEnumerable<ProveedorInventarioDTO>> GetListaProveedorInventario()
+        {
+            if (_context.ProveedorInventarios == null)
+            {
+                return NotFound();
+            }
+
+            var query = from pi in _context.ProveedorInventarios
+                        join p in _context.Proveedors on pi.Idproveedor equals p.Idproveedor
+                        join i in _context.Inventarios on pi.Idproducto equals i.Idproducto
+                        select new
+                        {
+                            IdproveedorInventario = pi.IdproveedorInventario,
+                            Idproveedor = p.Idproveedor,
+                            Idproducto = i.Idproducto,
+                            NombreProveedor = p.Nombre,
+                            NombreProducto = i.Nombre
+                        };
+
+            List<ProveedorInventarioDTO> ProveedorInventarioLista = new List<ProveedorInventarioDTO>();
+
+            foreach (var proveedorInventario in query)
+            {
+                ProveedorInventarioLista.Add(
+                    new ProveedorInventarioDTO
+                    {
+                        IdproveedorInventario = proveedorInventario.IdproveedorInventario,
+                        Idproveedor = proveedorInventario.Idproveedor,
+                        Idproducto = proveedorInventario.Idproducto,
+                        NombreProveedor = proveedorInventario.NombreProveedor,
+                        NombreProducto = proveedorInventario.NombreProducto
+
+                    }
+
+                    );
+            }
+
+            if (ProveedorInventarioLista == null)
+            {
+                return NotFound();
+            }
+
+            return ProveedorInventarioLista;
         }
 
         // PUT: api/ProveedorInventarios/5
--- MueblesCormar_API/Models/DTOs/ProveedorInventarioDTO.cs
+++ MueblesCormar_API/Models/DTOs/ProveedorInventarioDTO.cs
@@ -5,5 +5,7 @@
         public int IdproveedorInventario { get; set; }
         public int Idproveedor { get; set; }
         public int Idproducto { get; set; }
+        public string NombreProveedor { get; set; } = null!;
+        public string NombreProducto { get; set; } = null!;
     }
 }
